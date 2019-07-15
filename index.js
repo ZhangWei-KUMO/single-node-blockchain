66cdb521ced02afbd85e944f34f75aba2f882223
张伟 <kansee@gmail.com>
2019-07-15 21:51:16 +0800

    add

---
+++ index.js
@@ -0,0 +1,17 @@
+const Blockchain = require("./blockchain");
+const Block = require("./block");
+
+let jsChain = new Blockchain();
+jsChain.addBlock(new Block(new Date(), {
+  title: "利群香烟",
+  location: "园区第二食堂",
+  amount: "18.5元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "停车费",
+  location: "保卫科",
+  amount: "4000元",
+}));
+
+console.log(JSON.stringify(jsChain, null, 4));
+console.log("Is blockchain valid? " + jsChain.checkValid());
