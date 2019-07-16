fa9cb8068243a4ff3890e6a7db158c18c79590f4
张伟 <kansee@gmail.com>
2019-07-16 11:46:42 +0800

    Add Replication readme

--- index.js
+++ index.js
@@ -12,6 +12,76 @@ jsChain.addBlock(new Block(new Date(), {
   location: "保卫科",
   amount: "4000元",
 }));
+jsChain.addBlock(new Block(new Date(), {
+  title: "利群香烟",
+  location: "园区第二食堂",
+  amount: "18.5元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "停车费",
+  location: "保卫科",
+  amount: "4000元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "利群香烟",
+  location: "园区第二食堂",
+  amount: "18.5元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "停车费",
+  location: "保卫科",
+  amount: "4000元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "利群香烟",
+  location: "园区第二食堂",
+  amount: "18.5元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "停车费",
+  location: "保卫科",
+  amount: "4000元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "利群香烟",
+  location: "园区第二食堂",
+  amount: "18.5元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "停车费",
+  location: "保卫科",
+  amount: "4000元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "利群香烟",
+  location: "园区第二食堂",
+  amount: "18.5元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "停车费",
+  location: "保卫科",
+  amount: "4000元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "利群香烟",
+  location: "园区第二食堂",
+  amount: "18.5元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "停车费",
+  location: "保卫科",
+  amount: "4000元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "利群香烟",
+  location: "园区第二食堂",
+  amount: "18.5元",
+}));
+jsChain.addBlock(new Block(new Date(), {
+  title: "停车费",
+  location: "保卫科",
+  amount: "4000元",
+}));
 
 console.log(JSON.stringify(jsChain, null, 4));
-console.log("Is blockchain valid? " + jsChain.checkValid());
+console.log("区块链是否合法? " + jsChain.checkValid());
